b3209e9660a4aed0069bbe51a15f2ecd3ecf0ee6
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2018-10-07 05:17:22 +1100

    Add basic user interface
    Exit code is not yet accurate on errors other that I/O errors.
    
    Also, the error messages could be displayed after the passphrase
    for better readability.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
-fn main() {
+extern crate dice;
+
+fn run_app() -> Result<(), String> {
     use std::io;
+    use std::io::Write;
+    use dice::wordlist;
 
     println!("Welcome to Dice!");
 
     // get user input
     let mut throws = String::new();
+    print!("Dice throws (as many as you want): ");
+    io::stdout().flush().expect("Failed to write line");
     io::stdin().read_line(&mut throws)
         .expect("Failed to read line");
 
-    // convert string to list of numbers
+    // convert string to valid list of numbers
     let split = throws.split(" ");
     let throws: Vec<&str> = split.collect();
-    let throws: Vec<u32> = throws.iter().map(|&t|
-        parse_throw(t)
+    let throws: Vec<Option<u32>> = throws.iter().map(|&t|
+        match wordlist::parse_throw(t) {
+            Ok(throw) => Some(throw),
+            Err(error) => match error {
+               wordlist::Error::IsNotNumber(input) => {
+                   eprintln!("Dice throws must be composed of numbers, '{}' looks like a typo.", input.trim());
+                   None
+               },
+               wordlist::Error::IsNotDieValue(input) => {
+                   eprintln!("Only dice values between 1 and 6 are valid, value '{}' in '{}' looks like a typo.", input, t.trim());
+                   None
+               }
+            },
+       }
     ).collect();
 
-    for s in &throws {
-        println!("{}", s)
-    }
+    // lookup words based on the dice throws
+    let words = throws.iter().map(|&throw|
+        wordlist::lookup(throw)
+    ).collect();
+
+    // display passphrase
+    println!("Passphrase: {}", dice::passphrase(&words));
+    Ok(())
 }
 
-fn parse_throw(throw: &str) -> u32 {
-    throw.trim().parse().expect("Please type a number!")
+fn main() {
+    ::std::process::exit(match run_app() {
+       Ok(_) => 0,
+       Err(err) => {
+           eprintln!("error: {:?}", err);
+           1
+       }
+    });
 }
