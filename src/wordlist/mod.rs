973b877ac7ebcd5816d31b131c6838c7eceab1b5
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2018-10-07 05:16:00 +1100

    Use Option type as lookup parameter

--- src/wordlist/mod.rs
+++ src/wordlist/mod.rs
@@ -7,20 +7,22 @@ mod tests {
     #[test]
     fn lookup_with_valid_throw_returns_word() {
         let expected = Some(String::from("abacus"));
-        assert_eq!(expected, lookup(11111));
+        assert_eq!(expected, lookup(Some(11111)));
 
         let expected = Some(String::from("abdomen"));
-        assert_eq!(expected, lookup(11112));
+        assert_eq!(expected, lookup(Some(11112)));
 
         let expected = Some(String::from("zoom"));
-        assert_eq!(expected, lookup(66666));
+        assert_eq!(expected, lookup(Some(66666)));
+
+        assert_eq!(None, lookup(None));
     }
 
     #[test]
     fn lookup_with_invalid_throw_returns_none() {
-        assert_eq!(None, lookup(0));
-        assert_eq!(None, lookup(11110));
-        assert_eq!(None, lookup(66667));
+        assert_eq!(None, lookup(Some(0)));
+        assert_eq!(None, lookup(Some(11110)));
+        assert_eq!(None, lookup(Some(66667)));
     }
 
     #[test]
@@ -62,7 +64,7 @@ pub enum Error {
     IsNotDieValue(u32),
 }
 
-pub fn lookup(throw: u32) -> std::option::Option<String> {
+pub fn lookup(throw: Option<u32>) -> Option<String> {
     use std::collections::HashMap;
 
     let throws = vec![11111, 11112, 66666];
@@ -70,10 +72,15 @@ pub fn lookup(throw: u32) -> std::option::Option<String> {
 
     let eff_long_wordlist: HashMap<_, _> = throws.iter().zip(words.iter()).collect();
 
-    let word = eff_long_wordlist.get(&throw);
-    match word {
-        None => None,
-        Some(s) => Some((**s).clone()),
+    match throw {
+        Some(throw) => {
+            let word = eff_long_wordlist.get(&throw);
+            match word {
+                None => None,
+                Some(s) => Some((**s).clone()),
+            }
+        },
+        None => return None,
     }
 }
 
